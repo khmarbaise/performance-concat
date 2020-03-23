8a666ee61cf117b96ab374dc402df996ff188b7c
Karl Heinz Marbaise <kama@soebes.de>
2020-03-23 20:53:48 +0100

    Run First execution

--- src/main/java/com/soebes/performance/streams/BenchmarkStreamConcat.java
+++ src/main/java/com/soebes/performance/streams/BenchmarkStreamConcat.java
@@ -1,11 +1,9 @@
 package com.soebes.performance.streams;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -15,41 +13,59 @@ import java.util.stream.Stream;
 
 @BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
+
 public class BenchmarkStreamConcat {
 
 
-  private List<FancyStuff> fancyStuffs;
-
-  public BenchmarkStreamConcat() {
-    fancyStuffs = IntStream.rangeClosed(1, 50)
-      .mapToObj(i -> new FancyStuff(new Element(i + 1), createList(i)))
-      .collect(Collectors.toList());
-
-  }
-
-  private List<Element> createList(int factor) {
-    return IntStream.rangeClosed(2, 50)
-      .mapToObj(i -> new Element(i * factor))
-      .collect(Collectors.toList());
-  }
-
   @Benchmark
-  public List<Element> with_new_arraylist() throws InterruptedException {
-      return fancyStuffs.stream().flatMap(item -> {
-          ArrayList<Element> objects = new ArrayList<>();
-          objects.add(item.getElement());
-          objects.addAll(item.getElements());
-          return objects.stream();
-      }).collect(Collectors.toList());
+  public List<Element> with_new_arraylist(Container content) throws InterruptedException {
+    return content.getFancyStuffs().stream().flatMap(item -> {
+      ArrayList<Element> objects = new ArrayList<>();
+      objects.add(item.getElement());
+      objects.addAll(item.getElements());
+      return objects.stream();
+    }).collect(Collectors.toList());
 
   }
 
   @Benchmark
-  public List<Element> with_stream_concat() {
-      return fancyStuffs.stream()
-        .flatMap(fs -> Stream.concat(Stream.of(fs.getElement()), fs.getElements().stream()))
+  public List<Element> with_stream_concat(Container content) {
+    return content.getFancyStuffs()
+      .stream()
+      .flatMap(fs -> Stream.concat(Stream.of(fs.getElement()), fs.getElements().stream()))
+      .collect(Collectors.toList());
+
+  }
+
+  @State(Scope.Thread)
+  public static class Container {
+    private List<FancyStuff> fancyStuffs;
+
+    public Container() {
+      this.fancyStuffs = Collections.emptyList();
+    }
+
+    @Setup(Level.Trial)
+    public void setup() {
+      fancyStuffs = IntStream.rangeClosed(1, 50)
+        .mapToObj(i -> new FancyStuff(new Element(i + 1), createList(i)))
         .collect(Collectors.toList());
+    }
 
+    @TearDown(Level.Trial)
+    public void tearDown() {
+      this.fancyStuffs = Collections.emptyList();
+    }
+
+    private List<Element> createList(int factor) {
+      return IntStream.rangeClosed(2, 50)
+        .mapToObj(i -> new Element(i * factor))
+        .collect(Collectors.toList());
+    }
+
+    public List<FancyStuff> getFancyStuffs() {
+      return fancyStuffs;
+    }
   }
 
 }
